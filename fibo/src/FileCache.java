aede25c9081459ae85cdd4fe6c66aa82375d5bba
Ruslan <rush.yu@gmail.com>
2022-03-03 09:39:44 +0500

    Refactor cache

--- fibo/src/FileCache.java
+++ fibo/src/FileCache.java
@@ -2,10 +2,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Cache {
+public class FileCache {
     private static Map<Integer, String> cache = new HashMap<>();
 
     public static void main(String[] args) {
+
+
 
         while (true) {
             String res = "";
@@ -24,47 +26,25 @@ public class Cache {
                 if (elem == 0 || elem == 1) System.out.println(elem + " elem  = " + elem);
                 if (elem < 0) System.out.println("Fibonacci number cannot be negative");
             }
-
         }
     }
 
     /**
      * Возвращает число Фибоначи
-     * Кэширует каждое cacheN вычисленное число
-     * ! Первый раз 1000000 элементов просчитывает за 1200 секунд
-     * ! Последующие запуски в среднем - 1с
-     * refactor - нарушен первый принцип SOLID,
-     * вынести операции с кэшем в отдельный метод
+     * Кэширует все вычисленные числа Фибоначи
+     * ! При использовании кэша в памяти, куча заканчивается примерно на 140000-м элементе
      * @param elem количество чисел, которые нужно вывести
      */
     public static String fibo (int elem){
         String a1 = "0";
         String a2 = "1";
         String res = "";
-        int start = 3;
-        int cacheN = 1000;
-        int prevKey = 0;
-        String prevVal = "";
 
-        // если в кэше есть ближайший просчитанный элемент,
-        // то начинаем считать от него
-        if (cache.containsKey(elem - (elem % cacheN))){
-            start = elem - (elem % cacheN) - 1;
-            a1 = cache.get(elem - (elem % cacheN) - 1);
-            a2 = cache.get(elem - (elem % cacheN));
-        }
-
-        for (int i = start; i <= elem; i++) {
+        for (int i = 3; i <= elem; i++) {
             res = allSum(a1, a2);
             a1 = a2;
             a2 = res;
-            // в кэш записываем каждый cacheN элемент
-            if (i % cacheN == 0) {
-                cache.put(prevKey, prevVal);
-                cache.put(i, res);
-            }
-            prevKey = i;
-            prevVal = res;
+            cache.put(i, res);
         }
 
         return res;
@@ -78,33 +58,12 @@ public class Cache {
     public static String allSum(String s1, String s2){
         StringBuilder res = new StringBuilder();
 
-        // добавляем нули в начале меньшей строки
-        int s1Len = s1.length();
-        int s2Len = s2.length();
-
-        if (s1Len > s2Len){
-            for (int i = s2Len ; i < s1Len; i++){
-                s2 = "0".concat(s2);
-            }
-        }
-        if (s2Len > s1Len){
-            for (int i = s1Len ; i < s2Len; i++){
-                s1 = "0".concat(s1);
-            }
-        }
-
-        /**
-         * Альтернативный способ добавление нуля в начало меньшей строки,
-         * работает только с числами Фибаначи,
-         * так как в последовательности, разрядность следующего числа
-         * может быть больше только на один символ
-         */
-//        if (s1.length() > s2.length()) {
-//            s2 = "0" + s2;
-//        }else if (s2.length() > s1.length()) s1 = "0" + s1;
+        if (s1.length() > s2.length()) {
+            s2 = "0" + s2;
+        }else if (s2.length() > s1.length()) s1 = "0" + s1;
 
 
-        int brain = 0; // в эту переменную будем складывать 1, если сумма чисел будет больше 9
+        int brain = 0;
         int numA;
         int numB;
         int c;
